0584238a7ffb7c122e4fbf1f30a9fedd53ee8543
Arav016 <aravjain2005@gmail.com>
2025-07-04 12:27:07 +0530

    login page ready

---
+++ path-pilot/src/app/page.js
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Head from 'next/head';
+import styles from './login.css';
+
+
+export default function Home() {
+  
+  return (
+    <div className="w-screen h-screen bg-emerald-100 flex flex-col items-center">
+      <div className = "logo">
+        <Image
+        src="/images/path-pilot.png" // starts from public/
+        alt="PathPilot Logo"
+        width={300} // set your desired width
+        height={100} // set your desired height
+      />
+      </div>
+      <div className="login">
+        <div className="login-category">
+          <button className="admin">Admin</button>
+          <button className="driver">Driver</button>
+        </div>
+        <div className="login-inputs">
+        <input
+          type="text"
+          placeholder="Name"
+          className="name-input"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="password-input"
+        />
+        </div>
+
+        <button className="login main-login">Login</button>
+
+        <div className="forgot-password">Forgot password?</div>
+        
+      </div>
+
+    </div>
+  );
+}
